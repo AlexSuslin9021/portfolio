de364d8e67d115fb406128abe37bdfe699a1e2a0
Alexsus <alexsuslin@inbox.ru>
2023-04-16 23:23:08 +0300

    update footer

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -2,20 +2,35 @@ import React from 'react'
 import s from './Footer.module.css';
 import styleContainer from '../common/styles/Container.module.css';
 import {Title} from "../title/Title";
+import git from '../assets/image/logo/github-icon-1.svg'
+import what from '../assets/image/logo/WAsvg.svg'
+import mail from    '../assets/image/logo/email.svg'
+import location from    '../assets/image/logo/location-pin-svgrepo-com.svg'
+
+
 
 
 export function Footer() {
     return (
         <div className={s.footerBlock}>
             <div className={`${styleContainer.container} ${s.footer} `}>
-                <Title title={'Alex Suslin'}/>
-                <div className={s.icon}>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
+                {/*<Title title={'Alex Suslin'}/>*/}
+                <div className={s.cont}>
+                <div className={s.iconContainer}>
+                    <div  className={s.icon}> <img src={what} alt=""/> <span> 8-905-771-16-16</span></div>
+                    <div  className={s.icon}> <img src={mail} alt=""/> <span> alexSuslin@inbox.ru</span></div>
+                    <div  className={s.icon}> <img src={location} alt=""/> <span> Moscow, Tatianin Park</span></div>
                 </div>
+                    <div className={s.nav}>
 
+                            <a href=""> About </a>
+                            <a href=""> My skills </a>
+                            <a href=""> Portfolio </a>
+                            <a href=""> Contact </a>
+
+
+                    </div>
+                </div>
                 <span className={s.bottom} >Все права защищены 2023</span>
 
 
