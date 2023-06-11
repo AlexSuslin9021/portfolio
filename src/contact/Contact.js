3cb3d9830af7e55a9a1df15057e13a945d2c6678
Alexsus <alexsuslin@inbox.ru>
2023-06-11 10:30:27 +0300

    add react email.js

--- src/contact/Contact.js
+++ src/contact/Contact.js
@@ -4,8 +4,24 @@ import styleContainer from '../common/styles/Container.module.css';
 import {Title} from "../title/Title";
 import {Description} from "../Description/Description";
 import Fade from 'react-reveal/Fade';
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 export function Contact() {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+debugger
+        e.preventDefault();
+
+        emailjs.sendForm('service_2xpisdm', 'template_2m5ekjm',  e.target  , '2zdn0LH42svXWP663')
+            .then((result) => {
+                alert( result.text);
+            }, (error) => {
+                alert(error.text);
+            });
+    };
+
     return (
         <div id={'contact'} className={s.contactBlock}>
             <div className={`${styleContainer.container} ${s.contact} `}>
@@ -14,15 +30,18 @@ export function Contact() {
                 <Description text={'If you are interested in my resume or you have any questions, you can use the form. I will be glad to discuss cooperation'}/>
                </Fade>
                 <Fade left>
-                <form action="" className={s.form}>
-                   <input  placeholder={'Name'} type="text"/>
-                   <input  placeholder={'Email'}  type="text"/>
-                    <textarea  placeholder={'Message'} ></textarea>
+                <form ref={form} onSubmit={sendEmail} className={s.form}>
+                   <input name='user_name'  placeholder={'Name'} type="text"/>
+                   <input name='user_email'   placeholder={'Email'}  type="email"/>
+                    <textarea name='message'  placeholder={'Message'} ></textarea>
+                    <input type="submit" value="Send" />
                 </form>
-                <button className={s.send}>Send message</button>
+                <button  className={s.send}>Send message</button>
                 </Fade>
             </div>
         </div>)
 }
+
+
 
 
